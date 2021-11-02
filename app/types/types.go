b0fad14d7402c43c82cdb13986427f2c03254953
Kamil Mukhametzyanov <grkamil@yandex.ru>
2021-11-02 20:13:39 +0300

    Revert "use workers for search"
    This reverts commit 2f8e45819ad49a9f6480b5f4848aecb3864a9978.

--- app/types/types.go
+++ app/types/types.go
@@ -2,12 +2,13 @@ package types
 
 import (
 	"github.com/MinterTeam/swap-router/app/swap"
+	"math/big"
 )
 
 type TradeSearch struct {
 	FromCoinId uint64
 	ToCoinId   uint64
 	TradeType  swap.TradeType
-	Amount     string
+	Amount     *big.Int
 	Trade      chan *swap.Trade
 }
