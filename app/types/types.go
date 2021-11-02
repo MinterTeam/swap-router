2f8e45819ad49a9f6480b5f4848aecb3864a9978
Kamil Mukhametzyanov <grkamil@yandex.ru>
2021-11-02 20:04:06 +0300

    use workers for search

--- app/types/types.go
+++ app/types/types.go
@@ -2,13 +2,12 @@ package types
 
 import (
 	"github.com/MinterTeam/swap-router/app/swap"
-	"math/big"
 )
 
 type TradeSearch struct {
 	FromCoinId uint64
 	ToCoinId   uint64
 	TradeType  swap.TradeType
-	Amount     *big.Int
+	Amount     string
 	Trade      chan *swap.Trade
 }
